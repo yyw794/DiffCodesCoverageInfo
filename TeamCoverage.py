999920ba3c25a5203c87d0e1eb0d55f8ebca9ae6
yyw794 <25162504@qq.com>
2016-12-31 00:29:19 +0800

    Add files via upload

--- TeamCoverage.py
+++ TeamCoverage.py
@@ -7,13 +7,15 @@ import os
 import argparse
 import yaml
 
-svn_diff_info_file_name = "svn_diff.txt"
-
 is_py_v2 = True if sys.version[0] == '2' else False
 is_linux = True if 'linux' in sys.platform else False
 if is_py_v2:
 	reload(sys)  
 	sys.setdefaultencoding('utf8')
+
+#content of conf.yaml
+global conf
+
 def exec_shell_return_all(order_list):
 	if not isinstance(order_list, list):
 		order_list = order_list.split()
@@ -32,223 +34,180 @@ def exec_shell(order_list):
 #print(stdout_str)
 	return stdout_str
 
-def OpenFile(FileName, OP):
-	try:
-		File = open(FileName, OP)	
-	except IOError,e:
-		print ("Failed to open File %s:%s" % (FileName, e))
-		sys.exit(1)
+def file_should_be_ingored(file_name, filter_files):
+	return True if filter_files is None or file_name in filter_files else False
 
-	return File
+def write_blank_html():
+        html_path = os.path.join(coverage_folder, 'index.html')
 
+        content = '''
+        <html>
+        	<head>
+            	<title> Coverage for Difference </title>
+            </head>
+            <body style="font-family:consolas;">
+                <h2> </h2>
+                <h1 style="border:1px solid #96c2f1;background:#eff7ff" align="left"
+                <br> Since {} 00:00:00 to now, there is no difference found. </br>
+                </h1>
+            </body>
+        </html>'''.format(datetime.date.today())
 
-def get_filter_files(filter_file):
-	filter_files = []
-	if filter_file is not None:
-		with open(filter_file, "r") as f:
-			filter_files = f.readlines()
-		filter_files = [x.strip() for x in filter_files]
-	return filter_files
+        with open(html_path, 'w') as f:
+        	f.write(content)
 
-
-
-def FileIsFilter(FileName, FilterFileList):
-	if len(FilterFileList) == 0:
-		return True	
-	
-	if FileName in FilterFileList:
-		return True
-	else:
-		return False 
-
-def WriteBlankInfo():
-        Cmd = "rm -rf %s" % (coverage_folder)
-        call(Cmd, shell=True)
-
-        os.mkdir(coverage_folder)
-
-        OutFileName = os.path.join(coverage_folder, 'index.html')
-
-        today = datetime.date.today()
-        StrList = ["<html>\n",
-                "<head>\n",
-                "<title> Coverage for Difference </title>\n",
-                "</head>\n",
-                "<body style=\"font-family:consolas;\">\n",
-                "<h2> </h2>\n",
-                "<h1 style=\"border:1px solid #96c2f1;background:#eff7ff\" align=\"left\">",
-                "<br> Since %s 00:00:00 to now, there is no difference found. </br> </h1>\n" % (today),
-                "</body>\n",
-                "</html>\n",
-                ]
-        OutFile = OpenFile(OutFileName, "w")
-        OutFile.writelines(StrList)
-        OutFile.close()
-
-
-def svn_diff_info(DiffFileName, conf):
-	svn_diff_str = exec_shell(['svn','diff','--no-diff-deleted','-r',"{{{}}}:HEAD".format(conf["base_date"]),conf["svn_url"]])
-	#'' or None ?
+def output_svn_diff_info():
+	'''
+	do not include the files which are deleted
+	'''
+	svn_diff_info = exec_shell(['svn','diff','--no-diff-deleted','-r',"{{{}}}:HEAD".format(conf["base_date"]),conf["svn_url"]])
 	return svn_diff_str
 
+def keep_interest_svn_diff_info(svn_diff_info):
+	file_name_symbol = "Index:"
+	interest_svn_diff_info = ''
+	blocks_by_file = svn_diff_info.split(file_name_symbol)
+	for block in blocks_by_file:
+		file_name = block.splitlines()[0].strip()
+		if not file_should_be_ingored(file_name, filter_files):
+			interest_svn_diff_info += file_name_symbol
+			interest_svn_diff_info += block
+	return interest_svn_diff_info
 
-def parse_svn_diff_info(svn_diff_str, filter_files):
-	StartLine = -1 
-	FileValid = 0
-	diff_info_dict = {}
-	svn_diff_lines = svn_diff_str.split('\n')
+def get_new_lines_no_in_each_file(svn_diff_info):
+	'''
+	svn diff info just like this:
+	Index: unit_test.sh
+	===================================================================
+	--- unit_test.sh	(revision 36)
+	+++ unit_test.sh	(revision 802)
+	@@ -1,16 +1,8 @@
+	...
+
+	explaination:
+	@@(old version) start_line, lines_count, (new version) start_line, lines_count@@
+
+	only add new lines no of each file to the output
+	'''
+	added_line_no = -1 
+	user_new_lines = {}
+	svn_diff_lines = svn_diff_info.splitlines()
+	file_name_symbol = "Index:"
 
 	for l in svn_diff_lines:
-		reobj =  re.match("Index:(.*)", l)
-		if reobj:
-			FileName = reobj.group(1).strip()
-			FileName = FileName.split(":")[-1].split("/")[-1]
-			
-			FileValid = 0	
-			if FileIsFilter(FileName, filter_files):
-				FileValid = 1 				
-				diff_info_dict[FileName] = [] 
-
+		if l.startswith(file_name_symbol):
+			file_name = os.path.basename(l[len(file_name_symbol):])
+			user_new_lines[file_name] = []
+		else if l[:3] == '+++' or l[:3] == '---' or re.match("[=]+$", l):
 			continue
+		else if re.match("@@ -(.*),(.*) \+(.*),(.*) @@", l)
+			added_line_no = int(reobj.group(3))
+		else if l.startswith('+'):
+			user_new_lines[file_name].append(added_line_no)
+		else if l.startswith(' '):
+			start_line += 1
 
-		if l[:3] == '+++' or l[:3] == '---' or re.match("[=]+$", l):
-			continue
-	
-		if FileValid == 0:
-			continue
-
-		reobj = re.match("@@ -(.*),(.*) \+(.*),(.*) @@", l)
-		if reobj:
-			StartLine = int(reobj.group(3))
-			#print "First: %d" % (StartLine)
-			continue
-
-		#print "%c: %d" % (l[0],StartLine)
-		if l[0] == '+':
-			if FileName in diff_info_dict:
-				diff_info_dict[FileName].append(StartLine)	
-
-		if l[0] != '-':
-			StartLine += 1
-
-	return diff_info_dict
+	return user_new_lines
 
 
-def user_diff_info_per_file(TNName, InfoDict):
+def user_diff_info_per_file(TN_name, InfoDict):
 	if len(InfoDict) == 0:
 		return ''
 
 	#TODO: tell what is TN DA LF LH
-	user_diff_info = "TN:{}\n".format(TNName)
+	user_diff_info = "TN:{}\n".format(TN_name)
 
 	for k, value in InfoDict.iteritems():
-		LFCount, LHCount = 0
+		LF_count, LH_count = 0
 		for l in value:
 			if l[:3] == "DA:":
 				seq = l.strip().split(":")[-1].split(",")
-				LFCount += 1 		
-				LHCount += int(seq[-1]) > 0
+				LF_count += 1 		
+				LH_count += int(seq[-1]) > 0
 			else if(l.strip() == "end_of_record"):
-				user_diff_info += "LF:{}\n".format(LFCount)
-				user_diff_info += "LH:{}\n".format(LHCount)
+				user_diff_info += "LF:{}\n".format(LF_count)
+				user_diff_info += "LH:{}\n".format(LH_count)
 			user_diff_info += l
 
 	return user_diff_info
 
-def user_coverage_info_all_files(coverage_info, svn_diff_info, user, user_lines):
-	with open(coverage_info, 'r') as f:
+def make_user_coverage_info(user, user_new_lines, user_all_lines):
+	'''
+	SF: file name
+	'''
+	coverage_info_path = os.path.join(conf["coverage_folder"], conf["coverage_info"])
+	with open(coverage_info_path, 'r') as f:
 		coverage_info_lines = f.readlines()
 
-	TNName = ""
+	TN_name = ""
 	user_diff_info = ""
 
 	for l in coverage_info_lines:
 		# TN means a new file parse
-		if l[:3] == "TN:":
-			user_diff_info += user_diff_info_per_file(TNName,InfoDict)
-			TNName = l[3:].strip()
-			InfoDict = {}
-		else if l[:3] == "SF:":
-			SF = l.strip().split(":")[-1].split("/")[-1]
-			if SF in svn_diff_info:
-				InfoDict[SF] = []	
-				InfoDict[SF].append(l)	
+		if l.startswith("TN:"):
+			TN_name = l[3:].strip()
+			user_diff_info += user_diff_info_per_file(TN_name,InfoDict)
+			coverage_lines = {}
+		else if l.startswith("SF:"):
+			file_name = os.path.basename(l[3:])
+			#user_new_lines = {"xx.file":[2,4,5...],"yy.file":[1,2,3]}
+			if file_name in user_new_lines:
+				coverage_lines[file_name] = []	
+				coverage_lines[file_name].append(l)	
 	
 			while(l.strip() != "end_of_record"):
-				if SF in InfoDict:
-					if l[:3] == "DA:":
-						Line = int(l.strip().split(":")[-1].split(",")[0])
-						if Line in svn_diff_info[SF] and (user is None or Line in user_lines[SF]):
-							InfoDict[SF].append(l)
-				l = DiffInfoInFileIter.next()
+				if file_name in coverage_lines:
+					if l.startswith("DA:"):
+						line_no = int(l[3:].split(",")[0])
+						if line_no in user_new_lines[file_name] and (user is None or line_no in user_lines[file_name]):
+							coverage_lines[file_name].append(l)
 			else:
-				if SF in InfoDict:
-					if len(InfoDict[SF]) > 1:
-						InfoDict[SF].append(l)
+				if file_name in coverage_lines:
+					if len(coverage_lines[file_name]) > 1:
+						coverage_lines[file_name].append(l)
 					else:
-						del InfoDict[SF]
+						del coverage_lines[file_name]
 	else:
-		user_diff_info += user_diff_info_per_file(TNName,InfoDict)	
+		user_diff_info += user_diff_info_per_file(TN_name,coverage_lines)	
 
 	return user_diff_info
 
 
-def write_user_coverage_info(coverage_info, svn_diff_info, user, user_lines):
-	user_diff_info = make_user_coverage_info(coverage_info, svn_diff_info, user, user_lines)
-		
-	with open("")	
+def create_user_coverage_info(user, user_new_lines, user_all_lines):
+	user_diff_info = make_user_coverage_info(user, user_new_lines, user_all_lines)
 
-	user_coverage_info = os.path.join(coverage_folder, user)
-	DiffInfoOutFile = OpenFile(DiffInfoOut, "w")	
+	user_coverage_info_path = os.path.join(coverage_folder, user+coverage_info)
+	with open(user_coverage_info_path, "w") as f:
+		f.write(user_diff_info)	
 
+	return user_coverage_info_path
 
-	DiffInfoOutFile.close()
-	
-	FileLen = os.stat(DiffInfoOut).st_size
-	if FileLen == 0:
-		print("No diff info")
-		os.remove(DiffInfoOut)
-		WriteBlankInfo()
-		sys.exit(0)
-	else:
-		for each in os.listdir(coverage_folder):
-			if each == coverage_diff_file or each == svn_diff_info_file_name:
-				continue
-			os.system("rm -rf {}".format(each))
-		#Cmd = "cd {} && shopt -s extglob && rm -rf !({}|{})".format(coverage_folder, coverage_diff_file, svn_diff_info_file_name)
-		genhtml_output = exec_shell(['genhtml','-o',coverage_folder, DiffInfoOut])
-		return genhtml_output
 
 def parse_conf():
-	conf = yaml.load(file('CoverageDifference.yaml'))
+	configure_file = 'Configure.yaml'
+	global conf
+	conf = yaml.load(file(configure_file))
+	print(conf)
 	if conf["users"] is None:
+		print("you should add users in {}".format(configure_file))
 		#log error
 		sys.exit(1)
-	print(conf)
-	return conf
-	'''
-	
-	coverage_info = conf["coverage_info"]
-	svn_url = conf["svn_url"]
-	base_time = conf["base_time"]
-	users = conf["users"]
-	filter_file = conf["filter_file"]
-	'''
 
-def get_all_c_files(svn_url):
+def get_c_files(svn_url):
 	cmd = "svn list --depth infinity {}".format(svn_url)
-	files = exec_shell(cmd)
-	#print(files)
-	files = files.split('\n')
+	files = exec_shell(cmd).splitlines()
 
-	c_files = []
-	for each in files:
-		if '.c' in each:
-			c_files.append(each)
+	c_files = [x for x in files if '.c' in x]
+
 	return c_files
 
-def get_user_LOC(svn_url, user):
-	all_c_files = get_all_c_files(svn_url)
+def get_user_LOC(user):
+	'''
+	dict info is  which c file contains which line belongs to the user
+	using svn blame command
+	'''
+	svn_url = conf["svn_url"]
+	all_c_files = get_c_files(svn_url)
 
 	user_lines = {}
 	for c_file in all_c_files:
@@ -256,10 +215,9 @@ def get_user_LOC(svn_url, user):
 		user_lines[c_file_name]=[]
 
 		c_file_svn_url = os.path.join(svn_url, c_file)
-		ret = exec_shell(['svn','blame', '-v', c_file_svn_url])
-		ret = ret.split('\n')
+		svn_blame_info = exec_shell(['svn','blame', '-v', c_file_svn_url]).splitlines()
 
-		for sn, line in enumerate(ret):
+		for sn, line in enumerate(svn_blame_info):
 			if line == '':
 				continue
 			line_sn = sn+1
@@ -271,27 +229,30 @@ def get_user_LOC(svn_url, user):
 
 	return user_lines
 
+def user_coverage_info_2_html(user, user_coverage_info_path):
+	'''
+	each user has its own folder which contains index.html
+	'''
+	if os.stat(user_coverage_info_path).st_size == 0:
+		write_blank_html()
+	#genhtml will mkdir output folder if not exists
+	exec_shell(['genhtml','-o',conf["coverage_info"]+user, user_coverage_info_path])
+
+def write_user_html(user):
+	user_all_lines = get_user_LOC(user) 
+
+	svn_diff_info = output_svn_diff_info()
+	interest_svn_diff_info = keep_interest_svn_diff_info(svn_diff_info)
+	user_new_lines = get_new_lines_no_in_each_file(interest_svn_diff_info)
+
+	user_coverage_info_path = create_user_coverage_info(user, user_new_lines, user_all_lines)
+	user_coverage_info_2_html(user, user_coverage_info_path)
+
 def main():
-	#filter_file, BaseTime, coverage_all_file, svn_url, user = parse_input()
-	conf = parse_conf()
+	parse_conf()
 
 	for user in conf["users"]:
-		user_lines = get_user_LOC(conf["svn_url"], user) 
-
-		svn_diff_str = svn_diff_info(conf)
-
-		diff_info_dict = parse_svn_diff_info(svn_diff_str, conf["filter_files"])
-
-		#TODO: combine this to the following
-		if diff_info_dict == {}:
-			WriteBlankInfo()
-			sys.exit(0)	
-
-		genhtml_output = make_user_coverage_info(conf["coverage_info"], diff_info_dict, user, user_lines)
-
-	#TODO:get coverage rate and write it to file and show 
-
+		write_user_html()
 
 if __name__ == '__main__':
-	parse_conf()
-	#main()
+	main()
